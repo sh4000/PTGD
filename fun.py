64628f737f0d204457198c998176ded7b794873e
*as4h <123255978+as4h@users.noreply.github.com>
2023-01-27 19:02:56 +0100

    Add files via upload

---
+++ fun.py
@@ -0,0 +1,18 @@
+#j'te ez dans tout avec python !
+#:joy:
+
+
+import turtle
+import random
+
+wn = turtle.Screen()
+wn.bgcolor("black")
+
+star = turtle.Turtle()
+
+for i in range(50):
+    star.color(random.random(), random.random(), random.random())
+    star.forward(i * 10)
+    star.right(144)
+
+wn.mainloop()
